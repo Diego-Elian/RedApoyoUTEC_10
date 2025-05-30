ab35d14713fce99526451b4449b2c164adbd874b
ValentinaVega34 <valentinavega807@gmail.com>
2025-05-30 18:46:54 -0300

    agregando comentarios

--- .idea/Psicopedagogo.java
+++ .idea/Psicopedagogo.java
@@ -1,5 +1,6 @@
 package RedApoyo;
 
+// Clase que representa a un estudiante
 public class Estudiante {
     private String nombreCompleto;
     private int cedula;
@@ -9,11 +10,13 @@ public class Estudiante {
     private String contraseña;
     private String necesidadEspecifica;
 
+    // Constructor que inicializa nombre y generación
     public Estudiante(String nombreCompleto, int generacion) {
         this.nombreCompleto = this.nombreCompleto;
         this.generacion = this.generacion;
     }
 
+     // Representación en texto del objeto Estudiante
     @Override
     public String toString() {
         return "Estudiante: " + nombreCompleto + ", generacion: " + generacion;
