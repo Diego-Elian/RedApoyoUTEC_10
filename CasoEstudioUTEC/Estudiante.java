9189094c277ddacbb0f6eb8de932790d0a01e7c0
Diego-Elian <diego.pineyro@estudiantes.utec.edu.uy>
2025-05-30 19:02:13 -0300

    Estudiante.java

---
+++ CasoEstudioUTEC/Estudiante.java
@@ -0,0 +1,46 @@
+//Clase  Estudiantes , heredada de Usuarios
+public class Estudiante extends Usuario {
+    private int Cedula;
+    String Carrera;
+    String Generacion;
+    String NecesidadEspecifica;
+
+//Metodo que llama al constructor desde Usuario
+    public Estudiante(String NombreCompleto, String Correo, String Contrasenia) {
+        super(NombreCompleto, Correo, Contrasenia);
+    }
+
+    //Metodo seters permite modificar valores
+    public void setCedula(int Cedula) {
+        this.Cedula = Cedula;
+    }
+    public void setCarrera(String Carrera) {
+        this.Carrera = Carrera;
+    }
+    public void setGeneracion(String Generacion) {
+        this.Generacion = Generacion;
+    }
+    public void setNecesidadEspecifica(String NecesidadEspecifica) {
+        this.NecesidadEspecifica = NecesidadEspecifica;
+    }
+
+    // Metodos getters permiten obtener los valores
+    public int getCedula() {
+        return Cedula;
+    }
+    public String getCarrera() {
+        return Carrera;
+    }
+    public String getGeneracion() {
+        return Generacion;
+    }
+    public String getNecesidadEspecifica() {
+        return NecesidadEspecifica;
+}
+
+// Método que devuelve una cadena de texto con los datos del estudiante
+
+    public String toString() {
+        return "Nombre :" + getNombre() + ", Correo: " + getCorreo() + ", Carrera: " + getCarrera() + ", Generacion: " + getGeneracion() + ", Necesidad Especifica: " + NecesidadEspecifica;
+}
+}
