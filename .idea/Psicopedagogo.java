801da9b9918f1b96dde22f74411429ee269c08bf
ValentinaVega34 <valentinavega807@gmail.com>
2025-06-13 20:08:39 -0300

    Correcciones y sugerencias realizadas y comentadas en Psicopedagogo

--- .idea/Psicopedagogo.java
+++ .idea/Psicopedagogo.java
@@ -5,21 +5,45 @@ public class Psicopedagogo extends Usuario {
     private String Especialidad;
 
     //Constructor del psicopedagogo
-    public Psicopedagogo(String NombreCompleto, String Correo, String Contrasenia) {
+    //Corrección hecha:se valida que los parámetros no sean nulos
+    public Psicopedagogo(String NombreCompleto, String Correo, String Contrasenia, String Especialidad) {
         super(NombreCompleto, Correo, Contrasenia);
-    }
-
-    //Metodo para establecer la especialidad
-    public void setEspecialidad(String Especialidad) {
+        if (NombreCompleto == null) {
+            throw new IllegalArgumentException("Nombre no puede ser nulo");
+        }
+        if (Correo == null) {
+            throw new IllegalArgumentException("Correo no puede ser nulo");
+        }
+        if (Contrasenia == null) {
+            throw new IllegalArgumentException("Contraseña no puede ser nula");
+        }
+        if (Especialidad == null) {
+            throw new IllegalArgumentException("Especialidad no puede ser nula");
+        }
+        
+        
+    
+ // Metodo para establecer la especialidad
         this.Especialidad = Especialidad;
     }
+
+    //corrección hecha: se valida que el parámetro no sea nulo
+    public void setEspecialidad(String Especialidad) {
+        if (Especialidad == null) {
+            throw new IllegalArgumentException("Especialidad no puede ser nula");
+        }
+        this.Especialidad = Especialidad; 
+    }
+    
     //Metodo para devolver la especialidad
     public String getEspecialidad() {
         return Especialidad;
     }
 
     //Metodo para devolver cadena de texto con el nombre, correo y especialidad del psicopedagogo
-    public String ToString(){
+    //corrección hecha: se agrega el override y se pone le pone minúscula a "toString"
+    @Override
+    public String toString(){
         return "Nombre :" + getNombreCompleto() + ", Correo: " + getCorreo() + ", Especialidad: " + getEspecialidad();
     }
 }
