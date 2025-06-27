733bcea8aa98fa9c2b51d1d6e8c11b8eb1eb93a6
Diego-Elian <diego.pineyro@estudiantes.utec.edu.uy>
2025-06-27 20:59:37 -0300

    Actualización Analista.java

--- CasoEstudioUTEC/Analista.java
+++ CasoEstudioUTEC/Analista.java
@@ -1,13 +1,13 @@
 // Clase Analista , heredada de Usuarios
 public class Analista extends Usuario {
-
     //Metodo que llama al constructor desde Usuario
-    public Analista(String NombreCompleto, String Correo, String Contrasenia) {
-        super(NombreCompleto, Correo, Contrasenia);
+    // Corrección hecha: se llama a super() como primera línea, se eliminan validaciones redundantes
+    public Analista(String nombreCompleto, String correo, String contrasenia) {
+        super(nombreCompleto, correo, contrasenia);
     }
-
     // Método que devuelve una cadena de texto con los datos
+    @Override
     public String toString() {
-        return "Nombre: " + getNombre() + ", Correo: " + getCorreo();
-}
+        return "Nombre: " + getNombreCompleto() + ", Correo: " + getCorreo();
+    }
 }
