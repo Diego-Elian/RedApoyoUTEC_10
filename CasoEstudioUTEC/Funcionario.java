26d52db24dcdc8a48945982df98cd0cefe5a94fe
Diego-Elian <diego.pineyro@estudiantes.utec.edu.uy>
2025-06-27 20:56:58 -0300

    Actualización Funcionario.java

--- CasoEstudioUTEC/Funcionario.java
+++ CasoEstudioUTEC/Funcionario.java
@@ -3,38 +3,32 @@ public class Funcionario extends Usuario {
     private String Rol;
     private String Profesion;
 
-   //Constructor que llama al de Usuario
+    //Constructor que llama al de Usuario
     public Funcionario(String NombreCompleto, String Correo, String Contrasenia){
         super(NombreCompleto,Correo,Contrasenia);
     }
+
     //Setters y getters para Rol
-    //Agregación de Validación 
     public void setRol(String rol){
-         if (Rol== null) {
-         throw new IllegalArgumentException("Error: Rol no válido.");
-        }
-        this.Rol=rol;
+        this.Rol = rol;
     }
+
     public String getRol(){
         return Rol;
     }
-    //Setters y getters para Profesion 
-    //Agregación de validación
-    public void setProfesion(String profesion){
 
-          if (Profesion== null) {
-            throw new IllegalArgumentException("Error: Profesión no válida.");
-        }
-        this.Profesion=profesion;
+    //Setters y getters para Profesion
+    public void setProfesion(String profesion){
+        this.Profesion = profesion;
     }
-        
+
     public String getProfesion(){
         return Profesion;
     }
+
     //Representación en texto del objeto
-        //Corrección de getNombre a getNombreCompleto
-     @Override
+    @Override
     public String toString(){
-        return "Nombre:" + getNombreCompleto() +", Correo:" + getCorreo()+", Rol:" +getRol() +", Profesion:" + getProfesion();
-    }
+        return "Nombre:" + getNombreCompleto() + ", Correo:" + getCorreo() + ", Rol:" + getRol() + ", Profesion:" + getProfesion();
+    }
 }
