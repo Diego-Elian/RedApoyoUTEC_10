e31c1a7f6be28d7325821336fb3232fa72ec941a
Diego-Elian <diego.pineyro@estudiantes.utec.edu.uy>
2025-06-27 20:56:14 -0300

    Actualización Psicopedagogo.java

--- CasoEstudioUTEC/Psicopedagogo.java
+++ CasoEstudioUTEC/Psicopedagogo.java
@@ -1,25 +1,35 @@
-package RedApoyo;
-
 // Clase Psicopedagogo que hereda de la clase Usuario
 public class Psicopedagogo extends Usuario {
     private String Especialidad;
 
     //Constructor del psicopedagogo
-    public Psicopedagogo(String NombreCompleto, String Correo, String Contrasenia) {
+    //Corrección hecha: se elimina validación redundante porque ya está en Usuario
+    public Psicopedagogo(String NombreCompleto, String Correo, String Contrasenia, String Especialidad) {
         super(NombreCompleto, Correo, Contrasenia);
-    }
-
-    //Metodo para establecer la especialidad
-    public void setEspecialidad(String Especialidad) {
+        if (Especialidad == null) {
+            throw new IllegalArgumentException("Especialidad no puede ser nula");
+        }
+        // Metodo para establecer la especialidad
         this.Especialidad = Especialidad;
     }
+
+    //corrección hecha: se valida que el parámetro no sea nulo
+    public void setEspecialidad(String Especialidad) {
+        if (Especialidad == null) {
+            throw new IllegalArgumentException("Especialidad no puede ser nula");
+        }
+        this.Especialidad = Especialidad; 
+    }
+    
     //Metodo para devolver la especialidad
     public String getEspecialidad() {
         return Especialidad;
     }
 
     //Metodo para devolver cadena de texto con el nombre, correo y especialidad del psicopedagogo
-    public String ToString(){
+    //corrección hecha: se agrega el override y se pone le pone minúscula a "toString"
+    @Override
+    public String toString(){
         return "Nombre :" + getNombreCompleto() + ", Correo: " + getCorreo() + ", Especialidad: " + getEspecialidad();
-    }
+    }
 }
