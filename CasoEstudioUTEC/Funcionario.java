8d4ff7b24228840e671447b209740878b856a2b1
saraiesquiera <sarai.esquiera@estudiantes.utec.edu.uy>
2025-06-27 22:29:51 -0300

    Actualización Funcionario.java

--- CasoEstudioUTEC/Funcionario.java
+++ CasoEstudioUTEC/Funcionario.java
@@ -4,21 +4,29 @@ public class Funcionario extends Usuario {
     private String Profesion;
 
     //Constructor que llama al de Usuario
-    public Funcionario(String NombreCompleto, String Correo, String Contrasenia){
+    public Funcionario(String NombreCompleto, String Correo, String Contrasenia, String Rol, String Profesion){
         super(NombreCompleto,Correo,Contrasenia);
+        this.Rol=Rol;
+        this.Profesion=Profesion;
+
     }
 
     //Setters y getters para Rol
     public void setRol(String rol){
-        this.Rol = rol;
+     if (rol == null) {
+    throw new IllegalArgumentException("Error: Rol no válido.");
     }
-
+      this.Rol = rol;
+    }
     public String getRol(){
         return Rol;
     }
 
     //Setters y getters para Profesion
     public void setProfesion(String profesion){
+  if (profesion == null) {
+      throw new IllegalArgumentException("Error: Profesión no válida.");
+  }
         this.Profesion = profesion;
     }
 
