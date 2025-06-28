ad2a215c01217fa76e4e873ca48e5cef6765e471
Diego-Elian <diego.pineyro@estudiantes.utec.edu.uy>
2025-06-27 23:07:59 -0300

    Actualizando Usuario.java

--- CasoEstudioUTEC/Usuario.java
+++ CasoEstudioUTEC/Usuario.java
@@ -1,77 +1,52 @@
-//Clase Usuario
 public class Usuario {
-
     private String NombreCompleto;
-
     private String Correo;
-
     private String Contrasenia;
 
-    //Constructor del Usuario
-    public Usuario(String NombreCompleto, String correo, String contrasenia) {
-
-        //Se agrega validación de constructores
-        if (NombreCompleto == null || NombreCompleto.isEmpty()) {
-            
-            throw new IllegalArgumentException("El nombre no puede ser nulo ni vacío");
+//Constructor del Usuario
+    public Usuario(String NombreCompleto, String Correo, String Contrasenia) {
+     //Se agrega validación de constructores
+     if (NombreCompleto == null ) {
+            throw new IllegalArgumentException("El nombre no puede ser nulo");
         }
-
-        if (correo == null || correo.isEmpty()) {
-
-            throw new IllegalArgumentException("El correo no puede ser nulo ni vacío");
+        if (Correo == null) {
+            throw new IllegalArgumentException("El correo no puede ser nulo");
         }
-
-        if (contrasenia == null || contrasenia.isEmpty()) {
-
-            throw new IllegalArgumentException("La contraseña no puede ser nula ni vacía");
-        }
-
+        if (Contrasenia == null) {
+            throw new IllegalArgumentException("La contraseña no puede ser nula");
+           }
         this.NombreCompleto = NombreCompleto;
+        this.Correo = Correo;
+     // Implementar encriptación de contraseña en futuras versiones.
+        this.Contrasenia = Contrasenia;
 
-        this.Correo = correo;
-
-        // Implementar encriptación de contraseña en futuras versiones.
-        this.Contrasenia = contrasenia;
     }
-
-    //Metodo para devolver nombre
-    
-    //Se modifica nombre del método 
+//Metodo para devolver nombre
+//Se modifica nombre del metodo 
     public String getNombreCompleto() {
-
         return NombreCompleto;
     }
-
-    // Metodo para agregar nombre
-
-    //Se modifica nombre del método
+// Metodo para agregar nombre
+//Se modifica nombre del metodo
     public void setNombreCompleto(String NombreCompleto) {
-
         this.NombreCompleto = NombreCompleto;
     }
-
-    // Metodo para devolver contraseña
+// Metodo para devolver contraseña
     public String getContrasenia() {
-
         return Contrasenia;
-
     }
-
-    // Metodo para agregar contraseña
+// Metodo para agregar contraseña
     public void setContrasenia(String Contrasenia) {
-
         this.Contrasenia = Contrasenia;
     }
 
-    //Metodo para devolver Correo
+//Metodo para devolver Correo
     public String getCorreo() {
-
         return Correo;
     }
-
+ 
     //Metodo para agregar correo
     public void setCorreo(String Correo) {
-        
-        this.Correo = Correo;
+        this.Correo = Correo;
     }
 }
